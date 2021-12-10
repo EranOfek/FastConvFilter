c9d4e4cbedde73ba8653b81dd03f471a41460401
EranOfek <ofek.eran@gmail.com>
2021-12-10 12:36:31 +0200

    Add files via upload
    2D Filtering an convolution utility
        FFT implementation and choose automatically between
        the matlab implementation and the fft implmentation based
        on speed considerations.
        + Convolution over NaN values.
    
           Original version is from the MAAT package: https://github.com/EranOfek/MAAT
           Most recent version from the AstroPack package: https://github.com/EranOfek/AstroPack
           FOR FULL FUNCTIONALITY YOU WILL NEED TO INSTALL THESE PACKAGES INSTEAD
     If you use this, please cite: Ofek (2014; ascl:1407.005)
    
       Usage eaxmples:
           See specific functions help.
           Note to remove the package names from the examples.

---
+++ filter2_fast.m
@@ -0,0 +1,59 @@
+function Res=filter2_fast(Mat1, Mat2, UseFFT, PadMethod)
+% Source/template detection in 2D images by filtering (cross-correlation) 
+% Package: imUtil.filter
+% Description: 2D filtering (cross correkation) of an image (matrix) with a
+%              template (filter). The program chooeses which algorith to
+%              use, either filtering via convolution or fft, which ever
+%              supposed to be faster given the filter size compared to the
+%              matrix size.
+% Input  : - A matrix.
+%          - A filter.
+%          - UseFFT: Method to use. [] - auto, otherwise logical. Default is [].
+%          - Padding before operation (padded region will be removed from
+%            output).
+%            '' - do nothing (Default).
+%            'circular' - circular boundry conditions.
+%            'replicate' - relpicate nearest edge value.
+%            'symmetric' - mirror reflection boundry conditions.
+% Output : - Convolution between the two matrices.
+% License: GNU general public license version 3
+% Tested : Matlab R2015b
+%     By : Eran O. Ofek                    Apr 2016
+%    URL : http://weizmann.ac.il/home/eofek/matlab/
+% Example: imUtil.filter.filter2_fast(rand(1000,1000),rand(1000,1000));
+% Reliable: 2
+%--------------------------------------------------------------------------
+
+
+arguments
+    Mat1
+    Mat2
+    UseFFT    = [];
+    PadMethod         = ''; % 'symmetric'; % circular | replicate | symmetric
+end
+
+Threshold = 0.004;  % requires calibration
+if isempty(UseFFT)
+    % auto method
+    UseFFT = (numel(Mat2)./numel(Mat1))>Threshold;
+end
+
+if ~isempty(PadMethod)
+    % pad Mat1
+    PadSize = size(Mat2);
+    Mat1    = padarray(Mat1, PadSize, PadMethod);
+end
+
+if UseFFT
+    % use fft
+    Res = filter2_fft(Mat1,Mat2);
+else
+    % note Mat2 is the first argument
+    Res = filter2(Mat2,Mat1,'same');
+end
+
+
+if ~isempty(PadMethod)
+    % unpad the Result
+    Res = unpad_array(Res, PadSize);
+end
